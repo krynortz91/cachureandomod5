752324871db9175bc7c39b430e0eff8b2cafab52
Miguel Rondanelli Nuñez <90483570+Izth4rDev@users.noreply.github.com>
2023-03-28 10:14:26 -0300

    Miguel (#8)
    * prueba
    
    * ya lo arregle oe
    
    * Añadida link del script prueba de metodos para traer datos de la API
    
    * Rama Miguel, añadido: funciones BorrarProducto-modificarProducto-agregarProducto para manipular data de la API

--- js/cart.js
+++ js/cart.js
@@ -2,15 +2,14 @@ import {Producto} from '/js/Producto.js';
 import {Carrito} from '/js/Carrito.js';
 
 const notebook=new Producto('imagen','notebook','123456','125Gb','200000',8,10);
-console.table(notebook)
+//console.table(notebook)
 const carrito= new Carrito('productos');
-console.table(carrito)
-let URLSucursal = 'https://slifer.bsite.net/td-sucursal';
-let URLCategoria = 'https://slifer.bsite.net/td-categoria';
+//console.table(carrito)
+
 let URLProductos = 'https://slifer.bsite.net/td-producto';
+let URLProductosAdd = 'https://slifer.bsite.net/td-producto';
 let datos;
-
-
+/*
 async function sucursal(){
 
     let respuesta = await fetch(URLSucursal);
@@ -18,21 +17,115 @@ async function sucursal(){
     console.log(datos);
 
 }
+
 async function categoria(){
 
     let respuesta = await fetch(URLCategoria);
     datos = await respuesta.json();
     console.table(datos);
 
-}
+}*/
 
+/*
 async function producto(){
 
     let respuesta = await fetch(URLProductos);
     datos = await respuesta.json();
     console.table(datos);
 
+}*/
+
+//Mostrar Productos
+async function producto(){
+    try{
+        const respuesta = await fetch(URLProductos);
+        datos = await respuesta.json();
+        console.table(datos);
+
+    }catch(error) {
+
+        console.log('error'+error);
+    }      
 }
-sucursal();
-categoria();
+
+
+//Agregar Productos
+/*async function agregarProducto(){
+
+    try{
+    const respuesta = await fetch(URLProductos, { 
+    method: 'POST',
+    credentials: 'same-origin', 
+    headers: { 'Content-Type': 'application/json' }, 
+    body: JSON.stringify({
+        "id": Producto.id,
+        "nombre": "prueba",
+        "precio": 0,
+        "link": "/prueba",
+        "stock": 4,
+        "etiqueta": "prueba",
+        "descripcion": "prueba",
+        "idCategoria": 6,
+        "idSucursal": 6
+      })  
+}); 
+
+    datos = await respuesta.json()
+    console.log(datos);
+
+    }catch(error){
+        console.log('Error: '+error);
+    }
+}*/
+
+//Borrar producto
+
+let id = '1007';
+
+async function borrarProducto(id){
+    try{                     
+        const respuesta = await fetch(`${URLProductosAdd}/${id}`, { 
+        method: 'DELETE'}); 
+    
+        datos = await respuesta.json()
+        console.log(datos);
+    
+    }catch(error){
+            console.log(`Error: ${error}`);
+    }
+}
+
+
+//Modificar productos
+async function modificarProducto(){
+
+    try{
+        const respuesta = await fetch(URLProductos,{ 
+            method: 'PUT',
+            credentials: 'same-origin', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({
+                "id": 1009,
+                "nombre": "Almohada de plumas de ganso",
+                "precio": 20000,
+                "link": "/img/asdad",
+                "stock": 4,
+                "etiqueta": "Almohada",
+                "descripcion": "Almohada suavecita de plumas de ganso blancas",
+                "idCategoria": 40,
+                "idSucursal": 6
+              })  
+        }); 
+    }catch(error){
+        console.log(`Error: ${error}`);
+    }
+
+}
+
+//sucursal();
+//categoria();
+
 producto();
+borrarProducto(id);
+modificarProducto()
+//agregarProducto();
